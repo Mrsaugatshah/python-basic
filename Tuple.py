a5b3155b247ae4102a965b5189a26d15e11eaa65
Mrsaugatshah <saugats082@gmail.com>
2026-01-20 07:43:34 +0545

    tuple learning for begainner

---
+++ Tuple.py
@@ -0,0 +1,51 @@
+# mytuple = ("apple", "banana", "cherry")#unchangeable
+# print(mytuple[2])
+
+
+# thistuple = ("apple", "banana", "cherry")
+# print(len(thistuple))
+
+
+# thistuple = ("apple",)
+# print(type(thistuple))
+
+# thistuple = ("apple", "banana", "cherry")
+# y = list(thistuple)
+# y.append("orange")
+# thistuple = tuple(y)
+
+# fruits = ("apple", "banana", "cherry", "strawberry", "raspberry")
+
+# (green, yellow, *red) = fruits
+
+# print(green)
+# print(yellow)
+# print(red)
+
+
+# thistuple = ("apple", "banana", "cherry")
+# for x in thistuple:
+#   print(x)
+
+# thistuple = ("apple", "banana", "cherry")
+# for i in range(len(thistuple)):
+#   print(thistuple[i])
+
+
+# thistuple = ("apple", "banana", "cherry")
+# i = 0
+# while i < len(thistuple):
+#   print(thistuple[i])
+#   i += 1
+
+# tuple1 = ("a", "b" , "c")
+# tuple2 = (1, 2, 3)
+
+# tuple3 = tuple1 + tuple2
+# print(tuple3)
+
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
+
