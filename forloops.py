fc32bef081f1ddb6fd001de71fc9a1888724b8b3
Mrsaugatshah <saugats082@gmail.com>
2026-01-24 08:36:33 +0545

    loops and match

---
+++ forloops.py
@@ -0,0 +1,20 @@
+# fruits = ["apple", "banana", "cherry"]
+# for x in fruits:
+#   print(x)
+
+
+# for x in range(6):
+#   if x == 3: break
+#   print(x)
+# else:
+#   print("Finally finished!")
+
+
+
+
+adj = ["red", "big", "tasty"]
+fruits = ["apple", "banana", "cherry"]
+
+for x in adj:
+  for y in fruits:
+    print(x, y)
