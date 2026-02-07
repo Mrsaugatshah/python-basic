71fea1c5e2962f7eb6f1ada261f61a530595676e
Mrsaugatshah <saugats082@gmail.com>
2026-02-07 11:19:15 +0545

    range

---
+++ range.py
@@ -0,0 +1,31 @@
+# x = range(3, 10, 2)
+
+# #display x:
+# print(x)
+
+# #convert to list to display the content of x:
+# print(list(x))
+
+
+
+# for x in range(10):
+#   print(x) 
+
+
+
+# print(list(range(5)))
+# print(list(range(1, 6)))
+# print(list(range(5, 20, 3)))
+
+
+# r = range(10)
+# print(r[2])
+# print(r[:3])
+
+
+r = range(0, 10, 2)
+print(list(r))
+print(6 in r)
+print(7 in r)
+
+
