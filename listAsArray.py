e98f5192e465c45989bf6d6a6f8dfcd053029520
Mrsaugatshah <saugats082@gmail.com>
2026-02-11 09:53:42 +0545

    listasArray

---
+++ listAsArray.py
@@ -0,0 +1,30 @@
+# cars = ["Ford", "Volvo", "BMW"]
+
+# for x in cars:
+#   print(x)
+
+
+# cars = ["Ford", "Volvo", "BMW"]
+# cars.append("Honda")
+# print(cars)
+
+
+
+# cars = ["Ford", "Volvo", "BMW"]
+# cars.append("Honda")
+# print(cars)
+
+
+
+cars = ["Ford", "Volvo", "BMW"]
+cars.remove("Volvo")
+print(cars)
+
+
+
+
+
+
+
+  
+  
